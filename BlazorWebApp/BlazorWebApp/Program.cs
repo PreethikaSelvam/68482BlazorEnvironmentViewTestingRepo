c4d28fecdb46b6b0859d6d9e19be28f89f85d6a3
PreethikaSelvam <preethika.selvam@syncfusion.com>
2026-08-19 19:00:24 +0530

    Test sample and evidences.

---
+++ BlazorWebApp/BlazorWebApp/Program.cs
@@ -0,0 +1,34 @@
+using BlazorWebApp.Client.Pages;
+using BlazorWebApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.MapStaticAssets();
+var razorComponents = app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(BlazorWebApp.Client._Imports).Assembly);
+
+if (Environment.GetEnvironmentVariable("CLIENT_ENVIRONMENT_OVERRIDE") is { Length: > 0 } clientEnvironment)
+{
+    razorComponents.WithBrowserOptions(options => options.WebAssembly.EnvironmentName = clientEnvironment);
+}
+
+app.Run();
